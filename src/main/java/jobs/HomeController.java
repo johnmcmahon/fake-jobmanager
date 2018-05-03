b24856d000361a4595f61d784c3f96036f3e3177
John McMahon <jmcmahon@radiantblue.com>
2018-05-03 13:24:41 -0400

    fixes

--- src/main/java/jobs/HomeController.java
+++ src/main/java/jobs/HomeController.java
@@ -1,16 +1,16 @@
 package jobs;
 
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
     @RequestMapping(value = "/jobs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String jobs() {
-        return "[{\"name\"=\"job1\"}, {\"name\"=\"job2\"}]";
+        return "[{\"name\":\"job1\"}, {\"name\":\"job2\"}]";
     }
 
 }
