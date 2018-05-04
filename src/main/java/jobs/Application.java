f2687590bb544ad985695128701c32da3714bb15
John McMahon <jmcmahon@radiantblue.com>
2018-05-03 22:38:05 -0400

    Tweaks

--- src/main/java/jobs/Application.java
+++ src/main/java/jobs/Application.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@SpringBootApplication
 @Configuration
 @ComponentScan(basePackages = { "jobs" })
 @EnableAutoConfiguration
--- src/main/java/jobs/HomeController.java
+++ src/main/java/jobs/HomeController.java
@@ -3,6 +3,7 @@ package jobs;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.MediaType;
@@ -15,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
     @Autowired
-    DiscoveryClient discoveryClient;
+    private DiscoveryClient discoveryClient;
+
+    @Value("${eureka.client.serviceUrl.defaultZone}")
+    private String eurekaUri;
 
     @RequestMapping(value = "/jobs", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public String jobs() {
-        return "[{\"name\":\"job1\"}, {\"name\":\"job2\"}]";
+        System.out.println("Eureka URI: " + eurekaUri);
+        return "[{\"name\":\"job1\"}, {\"name\":\"job2\"}, {\"name\":\"" + eurekaUri + "\"}]";
     }
 
     @RequestMapping("/service-instances/{applicationName}")
