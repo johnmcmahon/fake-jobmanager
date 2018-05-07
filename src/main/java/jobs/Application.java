f0667b11c7d3b7f17974ac5d1307043386cd6e6c
John McMahon <jmcmahon@radiantblue.com>
2018-05-07 09:02:09 -0400

    Config testing

--- src/main/java/jobs/Application.java
+++ src/main/java/jobs/Application.java
@@ -3,6 +3,7 @@ package jobs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(basePackages = { "jobs" })
 @EnableAutoConfiguration
 @EnableDiscoveryClient
+//@EnableOAuth2Sso
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
